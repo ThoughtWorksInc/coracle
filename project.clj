c21e38df26e0430372d36aa01f05006be4b30c96
John Cowie <jcowie@thoughtworks.com>
2015-09-21 14:38:00 +0100

    JC | Updated mongo config so it can also work with docker

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: describe your microservice"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot coracle.core
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring "1.3.2"]
@@ -15,10 +15,16 @@
                  [prismatic/schema "1.0.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.6.3"]
-                                  [ring-mock "0.1.5"]]
-                   :plugins [[lein-midje "3.1.1"]
-                             [lein-environ "1.0.1"]]
-                   :env {:app-host "0.0.0.0"
-                         :app-port "6000"
-                         :mongo-uri "mongodb://localhost:27017/coracle"}}})
+             :dev     {:dependencies [[midje "1.6.3"]
+                                      [ring-mock "0.1.5"]]
+                       :plugins      [[lein-midje "3.1.1"]
+                                      [lein-environ "1.0.1"]]
+                       :env          {:app-host   "0.0.0.0"
+                                      :app-port   "6000"
+                                      :mongo-host "localhost"
+                                      :mongo-port "27017"
+                                      :mongo-db   "coracle"}}
+             :docker  {:env {:app-host   "0.0.0.0"
+                             :app-port   "7000"
+                             :mongo-port 27017
+                             :mongo-db   "coracle"}}})
--- src/coracle/config.clj
+++ src/coracle/config.clj
@@ -3,6 +3,22 @@
 
 (defn app-port [] (Integer. (e/env :app-port)))
 (defn app-host [] (e/env :app-host))
-(defn mongo-uri [] (e/env :mongo-uri))
+
+(defn mongo-port [] (e/env :mongo-port))
+
+(defn mongo-container-tcp [port]
+  (let [k (-> (format "mongo-port-%s-tcp-addr" port) keyword)]
+    (e/env k)))
+
+(defn mongo-host []
+  (if-let [host (e/env :mongo-host)]
+    host
+    (mongo-container-tcp (mongo-port))))
+
+(defn mongo-db []
+  (e/env :mongo-db))
+
+(defn mongo-uri []
+  (format "mongodb://%s:%s/%s" (mongo-host) (mongo-port) (mongo-db)))
 
 
