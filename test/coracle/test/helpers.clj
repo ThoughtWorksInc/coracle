82de5a164bff4e0d6379bf8ea9065454c99f7fac
Christoph Wong <chwong@thoughtworks.com>
2015-09-30 16:59:14 +0100

    DM + CW | 225 | Ensure test-db connection is closed on each test

--- test/coracle/test/helpers.clj
+++ test/coracle/test/helpers.clj
@@ -1,10 +1,13 @@
 (ns coracle.test.helpers
   (:require [monger.core :as m]))
 
+(defn finalise-db-interaction [conn]
+  (m/drop-db conn "coracle-test")
+  (m/disconnect conn))
+
 (defn with-db-do [thing-to-do]
   (let [{:keys [db conn]} (m/connect-via-uri "mongodb://localhost:27017/coracle-test")]
     (try
       (thing-to-do db)
-      (m/drop-db conn "coracle-test")
-     (catch Exception e
-       (m/drop-db conn "coracle-test")))))
+      (finally
+        (finalise-db-interaction conn)))))
