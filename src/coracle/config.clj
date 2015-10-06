6cbcc09702ac7b83171c7aacd9f50ddf42bc0f8f
John Cowie <jcowie@thoughtworks.com>
2015-10-06 16:41:50 +0100

    JC | Make mongodb_host optional (isn't required in docker)

--- src/coracle/config.clj
+++ src/coracle/config.clj
@@ -1,38 +1,38 @@
 (ns coracle.config
   (:require [environ.core :as e]))
 
-(defn get-env
+(defn get-required
   ([env-key default]
    (if-let [v (get e/env env-key default)]
      v
      (throw (Exception. (format "No value supplied for key [%s] and no default provided" env-key)))))
   ([env-key]
-    (get-env env-key nil)))
+    (get-required env-key nil)))
 
-(defn app-port [] (Integer. (get-env :port)))
-(defn app-host [] (get-env :host))
+(defn app-port [] (Integer. (get-required :port)))
+(defn app-host [] (get-required :host))
 
-(defn mongo-port [] (get-env :mongodb-port))
+(defn mongo-port [] (get-required :mongodb-port))
 
 (defn mongo-container-tcp [port]
   (let [k (-> (format "mongo-port-%s-tcp-addr" port) keyword)]
     (k e/env)))
 
 (defn mongo-host []
-  (if-let [host (get-env :mongodb-host)]
+  (if-let [host (:mongodb-host e/env)]
     host
     (if-let [h (mongo-container-tcp (mongo-port))]
       h
       (throw (Exception. "Host not specified, and environment variable with linked container host cannot be found.")))))
 
 (defn mongo-db []
-  (get-env :mongo-db))
+  (get-required :mongo-db))
 
 (defn mongo-uri []
   (format "mongodb://%s:%s/%s" (mongo-host) (mongo-port) (mongo-db)))
 
 (defn bearer-token []
-  (get-env :bearer-token ""))
+  (get-required :bearer-token ""))
 
 (defn secure? []
-  (= "true" (get-env :secure "false")))
+  (= "true" (get-required :secure "false")))
